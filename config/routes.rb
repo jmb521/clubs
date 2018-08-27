cae77a2164cad128ee3f034af4f33c05efb2f9dc
Jennifer Pazos <jmp521@gmail.com>
2018-08-27 01:49:25 -0500

    added route for user to be redirect_to when signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  
+
   resources :profiles
   resources :memberships
   resources :children
   resources :families
   devise_for :users, controllers: {
       sessions: 'users/sessions'
-    }
 
+    }
+  # resources :users do
+  #   get "/" => "users#home"
+  # end
+  get '/users/' => "users#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'application#home'
