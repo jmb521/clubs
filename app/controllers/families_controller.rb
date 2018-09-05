a3f74e6dd8e38349efa5d5522f89c17b2cefcbf9
Jennifer Pazos <jmp521@gmail.com>
2018-09-05 01:12:10 -0500

    added edit and show page for family

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,2 +1,27 @@
 class FamiliesController < ApplicationController
+
+  def index
+    @family = User.find_by(:id => params[:user_id].family)
+  end
+  def update
+
+    @family = Family.find(params[:id])
+    @family.update(family_params)
+    redirect_to user_family_path(@family)
+  end
+
+  def edit
+    @user = User.find_by(:id => params[:user_id])
+    @family = Family.find(params[:id])
+  end
+  def show
+    @family = Family.find(params[:id])
+  end
+
+
+  private
+
+  def family_params
+    params.require(:family).permit(:user_id, :user_birthday, :spouse, :spouse_birthday)
+  end
 end
--- db/migrate/20180827040426_create_families.rb
+++ db/migrate/20180827040426_create_families.rb
@@ -2,9 +2,10 @@ class CreateFamilies < ActiveRecord::Migration[5.2]
   def change
     create_table :families do |t|
       t.integer "user_id"
-      t.string "user_birthday"
+      t.datetime "user_birthday"
       t.string "spouse"
-      t.string "spouse_birthday"
+      t.datetime "spouse_birthday"
+      
       t.timestamps
     end
   end
