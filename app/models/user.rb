5057547c43b9664042223afb8cee3070e2e32dc8
Jennifer Pazos <jmp521@gmail.com>
2018-09-06 00:02:31 -0500

    added a former member role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_one :profile, :autosave => true
-         has_many :children, :autosave => true
-         has_one :family, :autosave => true
-         has_one :membership
+        has_one :profile, :autosave => true
+        has_many :children
+        has_one :family, :autosave => true
+        has_one :membership, :autosave => true
 
-         # after_create :build_membership_infos
-         after_create :create_profile
-         after_create :create_family
-         after_create :create_membership
 
-           enum role: [:applicant, :user, :admin]
+        after_create :create_family
+        after_create :create_membership
+        after_create :create_profile
+
+
+
+
+
+           enum role: [:former, :applicant, :member, :admin]
          after_initialize :set_default_role, :if => :new_record?
 
          def set_default_role
            self.role ||= :applicant
+         end
+
+         def is_admin
+           user.admin?
          end
 end
