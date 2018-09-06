d3dd57901dbce2432d062ea6ea6bb5ba4b65fc24
Jennifer Pazos <jmp521@gmail.com>
2018-09-05 23:23:09 -0500

    created profiles controller to allow the user to edit personal information

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,34 @@
 class ProfilesController < ApplicationController
+
+  def show
+    @profile = Profile.find(params[:id])
+
+  end
+
+  def edit
+
+    @profile = Profile.find(params[:id])
+
+  end
+
+  def update
+
+    @profile = Profile.find(params[:id])
+    @profile.update(profile_params)
+    @profile.save
+    redirect_to profile_path(@profile)
+    # @family = Family.find_by(:user_id => @profile.user_id)
+    # if !@family
+    #   redirect_to edit_user_family_path(@profile.user_id)
+    # else
+    #   redirect_to user_profile_path(@profile)
+    # end
+  end
+
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :address1, :address2, :city, :state, :zipcode, :phone, :user_id)
+  end
 end
