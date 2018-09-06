ea472b237498c5f544a1aaccc78b0bd89ecafefb
Jennifer Pazos <jmp521@gmail.com>
2018-09-05 23:22:23 -0500

    created before_action helper for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     users_path
   end
+
+  def is_admin
+
+    if !current_user || !current_user.admin?
+        redirect_to root_path
+    end
+  end
 end
