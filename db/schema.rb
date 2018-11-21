53a43930a5095a029706a17c585e360e07ae1b24
Jennifer Pazos <jmp521@gmail.com>
2018-11-21 00:21:50 -0600

    migrated admin_site_settings migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_051717) do
+ActiveRecord::Schema.define(version: 2018_11_19_065024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_site_settings", force: :cascade do |t|
+    t.string "page_name"
+    t.string "id_or_class_name"
+    t.string "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "children", force: :cascade do |t|
     t.integer "user_id"
