2d86b3eb8f20872b12c7539c5058da49ecfb5fcf
Jennifer Pazos <jmp521@gmail.com>
2018-12-12 01:13:43 -0600

    updated application.rb with sendgrid settings

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,15 @@ module Clubs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_url_options = { host: 'tcmottc.herokuapp.com', port: 3000 }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :sendgrid_actionmailer
+    config.action_mailer.sendgrid_actionmailer_settings = {
+      api_key: ENV['SENDGRID_API_KEY'],
+      raise_delivery_errors: true
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
