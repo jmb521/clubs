79766680b80064d443269332d64a7ae0f9c867fa
github username <github email address>
2019-06-26 22:09:06 -0500

    added membership resource to active_admin

---
+++ app/admin/memberships.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Membership do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :is_member, :user_id, :membership_paid, :member_id
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
